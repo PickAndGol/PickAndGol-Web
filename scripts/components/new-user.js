6f2bd7c9b11b6544dd297c8e523f64a1ac74e7cd
Javier Contreras <javi_ocon@yahoo.es>
2017-02-28 21:23:06 +0100

    feature(registerUser): créate new component for new user

---
+++ scripts/components/new-user.js
@@ -0,0 +1,37 @@
+/**
+ * Created by balate on 28/2/17.
+ */
+angular.module("pickandgol").component("newUser",{
+    bindings:{
+        $router:"<"
+    },
+    templateUrl: "views/user-register.html",
+
+    controller: function(ServiceBackend){
+
+        var self = this;
+
+        self.saveUser = function(name,password,email) {
+            var user = { name: name,
+                password:password,
+                email:email};
+            ServiceBackend.saveUser(user).then(function(response) {
+                console.log(user);
+                // $scope.user.push(response.data);
+                console.log("data....",response.data);
+                console.log("response full", response);
+
+                if(response.data.data.code === 400){
+                    console.log("Error: "+ response.data.data.code + " " + response.data.data.description);
+                    alert("Asegurate de completar todos los datos y que estos sean validos");
+                }else if (response.data.data.code === 409){
+                    console.log("Error: "+ response.data.data.code + " " + response.data.data.description);
+                    alert("ERROR: Conflicto con el email introducido, este email ya esta registrado");
+                }else{
+                    alert("Usuario "+ user.name +" registrado!! ");
+                }
+
+            });
+        };
+    }
+});
