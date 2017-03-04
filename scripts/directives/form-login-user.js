14c4e2d55084469c366e1158befb38a7ce06345d
Javier Contreras <javi_ocon@yahoo.es>
2017-03-04 12:47:00 +0100

    fea(login): create directive login-user

---
+++ scripts/directives/form-login-user.js
@@ -0,0 +1,21 @@
+/**
+ * Created by balate on 1/3/17.
+ */
+
+angular.module("pickandgol").directive("formLoginUser", function () {
+
+    return {
+        restrict: "EA",
+        templateUrl: "views/form-login-user.html",
+        scope:{
+            loginToClick: "&"
+        },
+        link: function(scope){
+            scope.notifyLogin = function(){
+                scope.loginToClick({    name: scope.name,
+                                    password: scope.password,
+                                        email: scope.email});
+            };
+        }
+    };
+});
