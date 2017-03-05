831a27f423fc9dca444309dfa3dcef127fedf370
Javier Contreras <javi_ocon@yahoo.es>
2017-03-05 21:26:17 +0100

    fea(new-event): add fixed values pending of create details bar and get token of webstorage.

--- scripts/components/new-event.js
+++ scripts/components/new-event.js
@@ -9,9 +9,17 @@ angular.module("pickandgol").component("newEvent",{
 
     controller: function(ServiceBackend){
 
+        //TODO:get user token of webStorage while use token the last sesion any user with potsman.
+        var usertoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4YmM2YzY1ZWRmZjJmY2Y1MDM1NDM1NiIsImlhdCI6MTQ4ODc0MzUzOSwiZXhwIjoxNDg4NzQ0OTc5fQ.wKL42hwwHA6M6CzsZwH5TfUEqbih33X8r31VpewP7_s";
+        //TODO:get id pub in details of bar, the user. Details Bar pending develop
+        var userpub = "58a35d76b3d7d12f284557b5";
+
         var self = this;
 
         self.saveEvent = function(name,date,description,category,pub,token) {
+            pub = userpub;
+            token = usertoken;
+
             var event = {   name: name,
                             date:date,
                             description:description,
