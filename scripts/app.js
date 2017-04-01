178207c8433bb3dbedbb9135628f7d468945107d
Alicia Daza <amdazacastillo@gmail.com>
2017-04-01 21:44:40 +0200

    Add angular google maps

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,18 @@ angular.module(
     "pickandgol",
     [
         "ngComponentRouter",
-        "angularUtils.directives.dirPagination"
-        //"dahr.ng-haversine",
-    ]);
+        "angularUtils.directives.dirPagination",
+        "uiGmapgoogle-maps"
+    ])
+    // Google maps configuration
+    .config(function (uiGmapGoogleMapApiProvider) {
+        console.log('uiGmapGoogleMa<pApiProvider',uiGmapGoogleMapApiProvider);
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyDzFRbyBLw75vAAOslsRP5Z7ejnz0uUnSo',
+            //v: '3.20', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization'
+        });
+    });
 
 // Configure provider $locationProvider.
 // Establish navigation model HTML5 for Single Page Application to work
