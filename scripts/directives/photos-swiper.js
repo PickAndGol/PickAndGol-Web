0902c8e6e2dc927f994113e8e87206269e4f3e0f
Alicia Daza <amdazacastillo@gmail.com>
2017-04-02 22:50:16 +0200

    Rename to photos -swiper

--- scripts/directives/photos-swiper.js
+++ scripts/directives/photos-swiper.js
@@ -1,17 +1,17 @@
 
 angular
     .module("pickandgol")
-    .directive("elementsSwiper", function () {
+    .directive("photosSwiper", function () {
         return {
             // With restrict we indicate how to use the directive
             // A as attribute of an HTML element
             // E as HTML element
             restrict: "EA",
             // Directive view
-            templateUrl: "views/elements-swiper.html",
+            templateUrl: "views/photos-swiper.html",
             // Define communication interface between directive and parent scope (Controller / component)
             scope: {
-                elements: "<"//,
+                photos: "<"//,
                 //onSelectedElement: "&" // Notify event to parent scope
             },
             // Establish directive logic or manipulate DOM in view
