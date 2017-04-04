8ad2288ff35e643af42b076d7d958260524093e2
Javier Contreras <javi_ocon@yahoo.es>
2017-04-04 20:32:40 +0200

    refac(new-event):change inject form

--- scripts/components/new-event.js
+++ scripts/components/new-event.js
@@ -1,67 +1,76 @@
 /**
  * Created by balate on 5/3/17.
  */
-angular.module("pickandgol").component("newEvent",{
-    bindings:{
-        $router:"<"
-    },
-    templateUrl: "views/event-register.html",
 
-    controller: function(eventsService,categoriesService){
-        // get token by sessionStorage
-        var usertoken = sessionStorage.getItem('pickandgolToken');
 
-        //TODO:get id pub in details of bar, the user. Details Bar pending develop
-        var userpub = "58c53a9292b33d06a10ca1f4";
+var ctrl = function (eventsService,categoriesService){
 
-        var self = this;
+    // get token by sessionStorage
+    var usertoken = sessionStorage.getItem('pickandgolToken');
 
-       /* categoriesService.getCategories().then(function(response){
-            //get categories
-            //self.categories = response.data.data.items.name;
-            var items = response.data.data;
+    //TODO:get id pub in details of bar, the user. Details Bar pending develop
+    var userpub = "58c53a9292b33d06a10ca1f4";
 
-            self.categories = response.data.data.items;
-            console.log(response);
-            console.log(items);
-            console.log(self.categories);
-        });*/
+    var self = this;
 
-        self.saveEvent = function(name,date,description,category,pub,token) {
-            pub = userpub;
-            token = usertoken;
-            //date = datee;
+    /* categoriesService.getCategories().then(function(response){
+     //get categories
+     //self.categories = response.data.data.items.name;
+     var items = response.data.data;
 
-            var event = {   name: name,
-                date:date,
-                description:description,
-                category:category,
-                pub:pub,
-                token:token};
-            eventsService.saveEvent(event).then(function(response) {
-                console.log(event);
-                console.log("data....",response.data);
-                console.log("response full", response);
-                var errorDescription = response.data.data.description;
-                var codeError =  response.data.data.code;
-                var nameEvent = event.name;
+     self.categories = response.data.data.items;
+     console.log(response);
+     console.log(items);
+     console.log(self.categories);
+     });*/
 
-                if(codeError=== 400){
-                    console.log("Error: "+ codeError + " " + errorDescription);
-                    alert("Error 400");
-                }else if (codeError=== 409){
-                    console.log("Error: "+ codeError + " " + errorDescription);
-                    alert("ERROR: 409");
-                }else if (codeError === 404){
-                    console.log("Error: "+ codeError+ " " + errorDescription);
-                    alert("ERROR: 404, debes de añadir un pub");
-                }else{
-                    alert("Evento "+ nameEvent +" creado!! ");
-                    //window.location.href= "/events";
-                }
-            });
-        };
-    }
-});
+    self.saveEvent = function(name,date,description,category,pub,token) {
+        pub = userpub;
+        token = usertoken;
+        //date = datee;
+
+        var event = {   name: name,
+            date:date,
+            description:description,
+            category:category,
+            pub:pub,
+            token:token};
+        eventsService.saveEvent(event).then(function(response) {
+            console.log(event);
+            console.log("data....",response.data);
+            console.log("response full", response);
+            var errorDescription = response.data.data.description;
+            var codeError =  response.data.data.code;
+            var nameEvent = event.name;
+
+            if(codeError=== 400){
+                console.log("Error: "+ codeError + " " + errorDescription);
+                alert("Error 400");
+            }else if (codeError=== 409){
+                console.log("Error: "+ codeError + " " + errorDescription);
+                alert("ERROR: 409");
+            }else if (codeError === 404){
+                console.log("Error: "+ codeError+ " " + errorDescription);
+                alert("ERROR: 404, debes de añadir un pub");
+            }else{
+                alert("Evento "+ nameEvent +" creado!! ");
+                window.location.href= "/events";
+            }
+        });
+    };
+}
+
+ctrl.$inject = ["eventsService",
+                "categoriesService"];
+angular
+    .module("pickandgol")
+    .component("newEvent", {
+        bindings: {
+            $router: "<"
+        },
+        templateUrl: "views/event-register.html",
+        controller: ctrl
+    });
+
 
 
