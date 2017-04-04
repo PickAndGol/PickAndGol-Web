9f104b98ab04b6dcbaa5cd8a0abe5b809db6b35d
Javier Contreras <javi_ocon@yahoo.es>
2017-04-04 20:53:24 +0200

    refact(recovery-password): change form inject

--- scripts/components/recover-password.js
+++ scripts/components/recover-password.js
@@ -1,28 +1,35 @@
 /**
  * Created by balate on 4/3/17.
  */
-angular.module("pickandgol").component("recover",{
+
+var ctrl = function (usersService) {
+    
+    var self = this;
+
+    self.recoverUser = function(email) {
+        var recover = {email:email};
+        usersService.recoverUser(recover).then(function(response) {
+            console.log(recover);
+            // $scope.user.push(response.data);
+            // console.log("data....",response.data);
+            recover = response.data.data;
+            console.log("response full", response);
+            //console.log("response data/data", response.data.data);
+            alert("email enviado, revise su correo por favor ");
+            window.location.href= "/events";
+        });
+    };
+};
+
+ctrl.$inject = ["usersService"];
+
+angular.module("pickandgol")
+    .component("recover",{
     bindings:{
         $router:"<"
     },
     templateUrl: "views/recover-password.html",
+    controller: ctrl
 
-    controller: function(usersService){
+    });
 
-        var self = this;
-        var recover;
-
-        self.recoverUser = function(email) {
-            var recover = {email:email};
-            usersService.recoverUser(recover).then(function(response) {
-                console.log(recover);
-                // $scope.user.push(response.data);
-               // console.log("data....",response.data);
-                recover = response.data.data;
-                console.log("response full", response);
-                //console.log("response data/data", response.data.data);
-
-            });
-        };
-    }
-});
