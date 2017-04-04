e015f93e8fca1062133560de17bd842d66f92da9
Javier Contreras <javi_ocon@yahoo.es>
2017-04-04 20:42:09 +0200

    refac(login): change inject form

--- scripts/components/login.js
+++ scripts/components/login.js
@@ -1,38 +1,32 @@
 /**
  * Created by balate on 1/3/17.
  */
-angular.module("pickandgol").component("newLogin",{
-    bindings:{
-        $router:"<"
-    },
-    templateUrl: "views/login.html",
 
-    controller: function (usersService){
+var ctrl = function (usersService) {
+    var self = this;
 
-        var self = this;
+    self.loginUser = function(password,email) {
+        var login = {
+            password:password,
+            email:email
+        };
+        usersService.loginUser(login).then(function(response) {
+            console.log(login);
 
-        self.loginUser = function(password,email) {
-            var login = {
-                password:password,
-                email:email
-            };
-            usersService.loginUser(login).then(function(response) {
-                console.log(login);
+            var loginData = response.data.code;
+            var idUser = response.data.id;
+            var nameUser = response.data.data.name;
+            var tokenUser = response.data.data.token;
+            var favoritePubs = response.data.data.favorite_pubs;
+            var errorDescription = response.data.data.description;
+            var codeError =  response.data.data.code;
+            var name = response.data.data.name;
+            var responseError = response.data.result;
 
-                var loginData = response.data.code;
-                var idUser = response.data.id;
-                var nameUser = response.data.data.name;
-                var tokenUser = response.data.data.token;
-                var favoritePubs = response.data.data.favorite_pubs;
-                var errorDescription = response.data.data.description;
-                var codeError =  response.data.data.code;
-                var name = response.data.data.name;
-                var responseError = response.data.result;
+            if (responseError === "ERROR"){
+                console.log("Error: "+ codeError + " " + errorDescription);
 
-                if (responseError === "ERROR"){
-                    console.log("Error: "+ codeError + " " + errorDescription);
-
-                    switch (codeError) {
+                switch (codeError) {
                     case 400:
                         alert("Asegurate de completar todos los datos y que estos sean validos");
                         break;
@@ -52,18 +46,30 @@ angular.module("pickandgol").component("newLogin",{
                     default:
                         alert("Error desconocido");
                         break;
-                    }
-
-                    return;
                 }
 
-                self.$router.navigate(["/Events"]);
-                if (typeof(Storage) !== "undefined"){
-                    sessionStorage.setItem("pickandgolToken", tokenUser);
-                }
-                alert("Usuario "+ response.data.data.name +" Logueado!! ");
+                return;
+            }
 
-            });
-        };
-    }
-});
+            self.$router.navigate(["/Events"]);
+            if (typeof(Storage) !== "undefined"){
+                sessionStorage.setItem("pickandgolToken", tokenUser);
+            }
+            alert("Usuario "+ response.data.data.name +" Logueado!! ");
+
+        });
+    };
+};
+
+ctrl.$inject = ["usersService"];
+
+angular
+    .module("pickandgol")
+    .component("newLogin", {
+        bindings: {
+            $router: "<"
+        },
+        templateUrl: "views/login.html",
+        controller: ctrl
+    });
+
